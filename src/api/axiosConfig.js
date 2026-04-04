89779166b30213f0e378f0353faf8c948c3913cb
desgee <destinygeorge93@gmail.com>
2026-04-05 00:11:56 +0100

    the front end baseurl

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.120.182.251:8080/api',
-    headers: {
-        'Content-Type': 'application/json',
+    baseURL:'http://3.120.182.251:8080/api'
     },
-});
+);
